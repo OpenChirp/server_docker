3653670bcca5b81ea9b42d2bc21b3deef7cf2d3c
Nuno Pereira <npereira@andrew.cmu.edu>
2017-06-09 21:32:37 +0100

    Fix to the commit and push script

--- commit-and-push-images.sh
+++ commit-and-push-images.sh
@@ -4,12 +4,12 @@
 #[ -z "$BASH_VERSINFO" ] && printf "\n\033[1;35m Please make sure you're using \"bash\"! Bye...\033[m\n\n" >&2 && exit 245
 
 # commits and pushes the folowing repos
-docker_repos="openchirp/web openchirp/rest openchirp/mqtt-tsdb-storage-service"
+docker_repos="serverdocker_web_1:openchirp/web serverdocker_rest_1:openchirp/rest serverdocker_mqtt-tsdb-storage-service_1:openchirp/mqtt-tsdb-storage-service"
 
 # edit these ate you convinience
 commit_author="Openchirp"
 commit_tag=version_1
-commit_comment="\"${commit_tag} Commit\""
+commit_comment="\"${commit_tag} commit.\""
 
 # prefix commit tag with a ":"
 ct=":${commit_tag}"
@@ -17,6 +17,8 @@ ct=":${commit_tag}"
 # commit and push repos
 for dr in $docker_repos
 do
-	docker commit -a "${commit_author}" -m "${commit_comment}" serverdocker_web_1 openchirp/web${ct}
-	docker push $dr
+	container=$(echo $dr | cut -f 1 -d :)
+	repo=$(echo $dr | cut -f 2 -d :)
+	docker commit -a "${commit_author}" -m "${commit_comment}" ${container} ${repo}${ct}
+	docker push ${repo}${ct}
 done
