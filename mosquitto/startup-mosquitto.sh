248dac5c7c5d7ce5c3c82ad55221d47f793b667d
Khushboo Bhatia <khush@cmu.edu>
2018-04-27 12:00:02 -0400

    More mosquitto related changes

--- mosquitto/startup-mosquitto.sh
+++ mosquitto/startup-mosquitto.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #
-echo "shit1"
 cd /etc/ssl/certs/
 chmod +x generate-CA.sh
 export HOSTLIST="mosquitto-server"
 # generate self-signed certificate using generate-CA.sh (from https://github.com/owntracks/tools/blob/master/TLS/generate-CA.sh)
 bash /etc/ssl/certs/generate-CA.sh mosquitto-server
 chmod 777 /etc/ssl/certs/*
-echo "shit2"
 # startup mosquitto
 
 mosquitto -c /etc/mosquitto/mosquitto.conf
