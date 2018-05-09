536693886c552be33bf8f89a715175724a1d67d7
Khushboo Bhatia <khush@cmu.edu>
2018-05-09 16:47:24 -0400

    Fixing typo in mapper startup script

--- gpsmapper-service/startup_service.sh
+++ gpsmapper-service/startup_service.sh
@@ -9,6 +9,6 @@ done
 cat /tokens/mapper_service.token >> /run/service.env
 # Set environment variable
 source /run/service.env
-export $(cut -d= -f1 /tokens/storage_service.token)
+
 # Start service
 /go/bin/gpsmapper-service
