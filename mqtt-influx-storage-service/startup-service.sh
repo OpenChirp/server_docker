230ad6c526e1a204cc5da4d2b4ad49d2b9c9ffc6
Khushboo Bhatia <khush@cmu.edu>
2018-05-08 22:48:35 -0400

    Reduced nodejs docker image and more changes for separate seed container

--- mqtt-influx-storage-service/startup-service.sh
+++ mqtt-influx-storage-service/startup-service.sh
@@ -1,72 +1,8 @@
 #!/bin/bash
 
-# TODO: Hackish.. Creating admin user in service since this is the first place we need this user.
-# Ideally admin user and other seeding of data should happen in a separate container
 
-# Create Admin User
-curl -v -H "Content-Type: application/json" \
-    -XPOST http://rest:10010/auth/signup \
-        -d @- <<EOF
-{
-    "email": "admin@localhost.com",
-    "name": "Openchirp Admin",
-    "password": "${ADMIN_PASSWORD}"
-}
-EOF
-
-# Get Session cookie
-curl -v -H "Content-Type: application/json" \
-    -XPOST http://rest:10010/auth/basic -c cookie.txt \
-            -d @- <<EOF
-{
-    "username": "admin@localhost.com",
-    "password": "${ADMIN_PASSWORD}"
-}
-EOF
-
-# Create admin group
-curl -v -H "Content-Type: application/json" \
-    -XPOST http://rest:10010/api/group -b cookie.txt \
-            -d @- <<EOF
-{
-    "name": "admin"
-}
-EOF
-
-# Create developer group
-curl -v -H "Content-Type: application/json" \
-    -XPOST http://rest:10010/api/group -b cookie.txt \
-                        -d @- <<EOF
-{
-    "name": "developer"
-}
-EOF
-
-# Create root location
-curl -v -H "Content-Type: application/json" \
-    -XPOST http://rest:10010/api/location -b cookie.txt \
-            -d @- <<EOF
-{
-    "name": "root",
-    "type": "OUTDOOR"
-}
-EOF
-
-# Create time series storage service
-SERVICE_ID=$(curl -s -H "Content-Type: application/json" \
-    -XPOST http://rest:10010/api/service -b cookie.txt \
-                --data '{"name": "Timeseries storage service", "description": "This service stores transducer data in influxdb"}'  | python -c "import sys, json; print(json.load(sys.stdin)['id'])" )
-
-
-# Create service token
-SERVICE_TOKEN=$(curl -s -H "Content-Type: application/json" \
-         -XPOST http://rest:10010/api/service/${SERVICE_ID}/token -b cookie.txt --data "{}")
-
-
-#Remove double quotes from start and end of token
-TOKEN=`sed -e 's/^"//' -e 's/"$//' <<<"$SERVICE_TOKEN"`
-
-
+#1. Wait for file to be available
+#2. Load variable from file
 #Update service ID and its token in service configuration file
 sed -e "s/SERVICE_ID/$SERVICE_ID/" /run/service.conf.start | \
     sed -e "s/PASSWORD/$TOKEN/" > /run/service.conf
--- node/startup-node.sh
+++ node/startup-node.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 sed -e "s/REST_MQTT_PASSWORD/$REST_MQTT_PASSWORD/" /docker.json.start > /usr/src/app/config/docker.json
   
